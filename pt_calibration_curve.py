05882cfd038d6de156d60a7d6826fad9523f14ee
herbie-c <74236333+herbie-c@users.noreply.github.com>
2025-05-12 15:36:09 +0200

    de data-analyse

--- pt_calibration_curve.py
+++ pt_calibration_curve.py
@@ -7,10 +7,19 @@ def get_temp(weerstand):
     (x1, y1) = (18.49, 73.15)
     (x2, y2) = (31.32, 103.15)
     temp = y1 + (y2-y1)/(x2-x1) * (weerstand - x1)
+    rico = (y2-y1)/(x2-x1)
     return temp
 #testing it with the table
-print(get_temp(100) - 273.15) #0
-print(get_temp(18.49) - 273.15) #-200
-print(get_temp(59.04)-273.15) #-103
-print(get_temp(40.96)-273.15) #-147
+print(get_temp(25.5))
 
+(x1, y1) = (18.49, 73.15)
+(x2, y2) = (31.32, 103.15)
+rico = (y2-y1)/(x2-x1)
+print('rico:', rico)
+
+
+
+
+
+print('gewenste weerstand:')
+print((90-y1)/rico + x1)
